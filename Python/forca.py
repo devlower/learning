7da1c5f2bb8d45f0f997ae997f8f378deb9b817c
Tuanne <tuanne.assenco@gmail.com>
2022-01-06 22:14:59 -0300

    Update lessons 06/01/2022

--- Python/forca.py
+++ Python/forca.py
@@ -4,23 +4,35 @@ def jogar():
     print('Bem-viado ao jogo de forca!')
     print('***************************')
 
-    palavra_secreta = 'banana'
+    palavra_secreta = 'irmão'.upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
 
+    print(letras_acertadas)
     # enquanto(True)
     while(not enforcou and not acertou):
 
-        chute = input('Qual letra? ')
-        index = 0
+        chute = input('Qual letra? ').upper().strip()
 
-        for letra in palavra_secreta:
-            if(chute == letra):
-                print(f'Encontrei a letra {letra} na posição {index}')
-            index = index + 1
-
-        print('Jogando...')
-
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    # print(f'Encontrei a letra {letra} na posição {index}')
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
+    if(acertou):
+        print('Você ganhou')
+    else:
+        print('Você perdeu')
     print('Fim do jogo')
 
 

c6b70c91367645aa8fa126f87c4ac2d25322d1cd
Tuanne Assenço <tuanne.assenco@gmail.com>
2022-02-22 22:54:04 -0300

    last lessons update

--- Python/forca.py
+++ Python/forca.py
@@ -1,11 +1,13 @@
+import random
+
+
 def jogar():
 
-    print('***************************')
-    print('Bem-viado ao jogo de forca!')
-    print('***************************')
+    imprime_mensagem_abertura()
 
-    palavra_secreta = 'lemniscata'.upper()
-    letras_acertadas = ['_' for letra in palavra_secreta]
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -34,6 +36,39 @@ def jogar():
     else:
         print('Você perdeu')
     print('Fim do jogo')
+
+
+def imprime_mensagem_abertura():
+    print('***************************')
+    print('Bem-viado ao jogo de forca!')
+    print('***************************')
+
+
+def carrega_palavra_secreta():
+
+    # *** abre e fecha o arquivo mesmo se der erro ***
+    #    with open('palavras.txt') as arquivo:
+    #        for linha in arquivo:
+    #            print(linha)
+
+    arquivo = open('palavras.txt', 'r')
+    lista_palavras = []
+
+    for item in arquivo:
+        item = item.strip()
+        lista_palavras.append(item)
+
+    arquivo.close()
+
+    index_palavra = random.randrange(0, len(lista_palavras))
+
+    palavra_secreta = lista_palavras[index_palavra].upper()
+    # print(lista_palavras[index_palavra].upper())
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra):
+    return ['_' for letra in palavra]
 
 
 if(__name__ == '__main__'):
---
+++ Python/list_comprehension_corrigido.py
@@ -0,0 +1,5 @@
+inteiros = [1, 3, 4, 5, 7, 8, 9]
+pares = [x for x in inteiros if x % 2 == 0]
+
+print(f'Os números da lista original são: {inteiros}')
+print(f'Os números PARES da lista original são: {pares}')

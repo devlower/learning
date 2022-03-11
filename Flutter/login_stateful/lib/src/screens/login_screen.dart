d6877ae3211ccbb134bc042f97cb76850d55976a
Tuanne Assenço <tuanne.assenco@gmail.com>
2022-03-11 09:02:08 -0300

    Updating Dart/Flutter learning progress

---
+++ Flutter/login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            emailField(),
+            passwordField(),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@exaple.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text('Submit!'),
+      style: ElevatedButton.styleFrom(
+        primary: Color.fromARGB(255, 96, 3, 150),
+      ),
+    );
+  }
+}
--- Flutter/pics/lib/src/app.dart
+++ Flutter/pics/lib/src/app.dart
@@ -1,12 +1,36 @@
 // Preciso importar um biblioteca ajudante do flutter para ter conteúdo
 // exposto na tela
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
 
 // Criar uma classe que será um widget customizado
 // Esta classe precisa extender a classe 'StalessWidget'
-class App extends StatelessWidget {
-  // Preciso definir o método 'build' que retornará os widgets que o *this* widget
-  // mostrará
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response = await get(
+        Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  // Preciso definir o método 'build' que retornará os widgets que o *this*
+  // widget mostrará
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
@@ -14,11 +38,10 @@ class App extends StatelessWidget {
           title: Text("Let's see some images"),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Hi there!');
-          },
-          child: Icon(Icons.anchor),
+          onPressed: fetchImage,
+          child: Icon(Icons.add),
         ),
+        body: ImageList(images),
       ),
     );
   }
